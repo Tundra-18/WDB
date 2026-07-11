9488163e5531391a44b6fa7fa13e2400c2cbe20c
Zaw Ye Naung <zawyenaung.818@gmail.com>
2026-07-11 14:09:30 +0630

    latest

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let myAge = 20;
+const myCountry = "Myanmar";
+
+let x = 10;
+let y = "5";
+let sum = x + Number(y); 
+
+
+console.log("Age:", myAge);
+console.log("Country:", myCountry);
+console.log("Calculated Sum (10 + 5):", sum);
+console.log("Type of myCountry:", typeof myCountry);
+
+
+
+
+
---
+++ js/test.js
@@ -0,0 +1,11 @@
+let result = 100 + 50 * 3;   // ၅၀ * ၃ ကို အရင်တွက်သဖြင့် ၂၅၀ ထွက်မည်။
+let result2 = (100 + 50) * 3; // ကွင်းထဲကို အရင်တွက်သဖြင့် ၄၅၀ ထွက်မည်။
+
+let text = "Hello";
+text += " World"; 
+console.log(text) // ရလဒ်: Hello World
+
+let five = 5
+five += "5"
+console.log(five) // ရလဒ်: 55
+
